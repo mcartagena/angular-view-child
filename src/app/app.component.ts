ef44181f24ff105d9e93887a64bfe806c1778425
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2023-02-06 05:31:41 -0300

    working with viewchild decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'view-child';
+
+  @ViewChild('dobInput')
+  deteOfBirth!: ElementRef<HTMLInputElement>;
+  @ViewChild('ageInput')
+  age!: ElementRef;
+  @ViewChild(DemoComponent, {static: true})
+  demoComp!: DemoComponent;
+
+  calculateAge(){
+    let birthYear = new Date(this.deteOfBirth.nativeElement.value).getFullYear();
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - birthYear;
+    this.age.nativeElement.value = age;
+
+    console.log(this.deteOfBirth);
+    console.log(this.age);
+  }
 }
